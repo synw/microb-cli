eb0314937bd1be852fcf96cb0e89e027da672ca9
Win <synwe@yahoo.com>
2018-03-06 15:34:39 +0000

    Fix command args passing

--- libmicrob/cmd/cmd.go
+++ libmicrob/cmd/cmd.go
@@ -1,7 +1,6 @@
 package cmd
 
 import (
-	"fmt"
 	m "github.com/synw/microb/libmicrob"
 	"github.com/synw/microb/libmicrob/types"
 )
@@ -23,6 +22,34 @@ func Use() *types.Cmd {
 func use(cmd *types.Cmd) *types.Cmd {
 	server := cmd.Args[0].(string)
 	m.Debug(server)
-	fmt.Println(len(cmd.Args), cmd.Args)
+	tr := serverExists(server_name)
+	if tr != nil {
+		tr = terr.Pass("comd.state.Use", tr)
+		tr.Error()
+		return
+	}
+	/*state.Server = state.Servers[server_name]
+	// init cli and check server
+	tr = state.InitServer()
+	if tr != nil {
+		err := errors.New("can not connect to websockets server: check your config")
+		tr := terr.Add("cmd.state.Use", err, tr)
+
+	} else {
+		msg := "Using server " + server_name
+		ctx.Println(msg)
+	}*/
 	return cmd
+}
+
+func serverExists(server_name string) *terr.trace {
+	for name, _ := range state.Servers {
+		if server_name == name {
+			return nil
+		}
+	}
+	msg := "Server " + server_name + " not found: please check your config file"
+	err := errors.New(msg)
+	tr := terr.New("ws.serverExists", err)
+	return tr
 }
